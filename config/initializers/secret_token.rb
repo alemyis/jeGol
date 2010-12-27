019359bf92539ed09398fe8047f6edee9975f33a
Alemeshet Alemu <alemyis@gmail.com>
2010-12-27 12:26:56 -0800

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JeGol::Application.config.secret_token = '237d264c44eb9e11edc0ae23ad6fc7f688d17419c59158fa0269eda9390253fb3e4e3bff65e000da9d36bdb4e7f20a939c493972c63a50a13b7aa55e8eb8d021'

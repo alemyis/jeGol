cf2ccf61262fb8d8adfc97307a97a6f52d0f0f20
Alemeshet Alemu <alemyis@gmail.com>
2011-01-10 04:51:31 -0800

    Playing with syslink to fix cap deploy issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ namespace :deploy do
   
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    #run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/jegol.yml #{release_path}/config/jegol.yml"
   end
 end

1e799b0139c907961a4d1a78074caef189194272
Alemeshet Alemu <alemyis@gmail.com>
2011-01-20 17:16:21 -0800

    Missing plugin file from last checkin

---
+++ public/javascripts/jegol.plugin.update.notify.js
@@ -0,0 +1,55 @@
+
+/**
+ * Register 'notify' update plugin
+ */
+JeGol.addUpdatePlugins('notify',
+{
+	/**
+	 * Update plugin - Audio visual inication for new messages.
+	 * @param {Object} msgStanza - string topic
+	 */
+     DoUpdate: function(msgStanza)
+     {
+		flashIfOutofScope();
+      	return false;
+     }
+});
+
+var browserInfocus = 1;
+var flashSpeed = 1500;
+var defaultTitle = 'jeGol';
+
+function onBlur() {
+	document.body.className = 'blurred';
+	browserInfocus = 0;
+}
+function onFocus(){
+	document.body.className = 'focused';
+	browserInfocus = 1;
+}
+
+function flashIfOutofScope(){
+	if (browserInfocus == 1)
+    {
+		window.document.title = defaultTitle;
+    }  
+  	else
+    {
+		if(window.document.title == defaultTitle)
+		{
+			window.document.title = '*New Message*';
+		}else
+		{
+			window.document.title = defaultTitle;
+		}
+		setTimeout("flashIfOutofScope();",flashSpeed);
+    }
+}
+
+if (/*@cc_on!@*/false) { // check for Internet Explorer
+	document.onfocusin = onFocus;
+	document.onfocusout = onBlur;
+} else {
+	window.onfocus = onFocus;
+	window.onblur = onBlur;
+}
